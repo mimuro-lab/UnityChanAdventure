ba0d16e5e71bebd8e33d7e1772dfb4a5af6ae80d
mimuro-lab <mimurosyunya@gmail.com>
2020-04-14 11:05:43 +0900

    Doxygenファイルの追加

---
+++ src/html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "mw",
+  1: "m",
+  2: "w"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "全て",
+  1: "ファイル",
+  2: "関数"
+};
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include "DxLib.h"
 
+/*!
+@brief プロジェクトのメイン関数
+@date 2020/04/14/11:03
+@author mimuro
+*/
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	DxLib_Init();	// DXライブラリ初期化処理
 	WaitKey();	// キー入力待ち
